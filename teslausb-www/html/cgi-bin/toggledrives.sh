3cbb233688abe0902f5996cfef740b122c78166c
msaxton99 <127694136+msaxton99@users.noreply.github.com>
2023-06-14 21:48:41 -0400

    Update toggledrives.sh

--- teslausb-www/html/cgi-bin/toggledrives.sh
+++ teslausb-www/html/cgi-bin/toggledrives.sh
@@ -2,8 +2,12 @@
 
 if [ -e "/sys/kernel/config/usb_gadget/teslausb/" ]
 then
+    echo 'disconnect from client and mount on host'
   sudo /root/bin/disable_gadget.sh
+  mount /mnt/lightshow
 else
+    echo 'unmount from host and mount on client'
+  umount -d /mnt/lightshow
   sudo /root/bin/enable_gadget.sh
 fi
 
